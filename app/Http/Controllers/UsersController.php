7244cff0d4fbb6cccdec58ed2c6bcad868effd75
EC2 Default User <testtest@gmail.com>
2018-08-22 05:08:09 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class UsersController extends Controller
 {
     public function index()
     {
-        $users = User::paginate(1);
+        $users = User::paginate(10);
 
-        return view('users.index', [
+            return view('users.index', [
             'users' => $users,
         ]);
     }
@@ -18,7 +20,7 @@ class UsersController extends Controller
         public function show($id)
     {
         $user = User::find($id);
-        
+        $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
         $data = [
             'user' => $user,
             'microposts' => $microposts,
